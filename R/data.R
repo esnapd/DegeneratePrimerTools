c3811980a20bea402a6a709e938e80880234b1c0
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-03-30 21:12:26 -0400

    data documentation

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Codon List for "Z" method of Degen.pl
+#'
+#' A named list of codons corresponding to the "Z"
+#' variables of the Degen.pl algorithm.
+#'
+#'
+#' @format A named list where names are codons sequences
+#' and the values are the degenerate codon for that sequence
+#' using the "Z" method of Degen.pl
+#'
+"degenZ"
+
+#' Codon List for "S" method of Degen.pl
+#'
+#' A named list of codons corresponding to the "S"
+#' variables of the Degen.pl algorithm.
+#'
+#'
+#' @format A named list where names are codons sequences
+#' and the values are the degenerate codon for that sequence
+#' using the "S" method of Degen.pl
+#'
+"degenS"
+
+#' Codon List for "SZ" method of Degen.pl
+#'
+#' A named list of codons corresponding to the "SZ"
+#' variables of the Degen.pl algorithm.
+#'
+#'
+#' @format A named list where names are codons sequences
+#' and the values are the degenerate codon for that sequence
+#' using the "SZ" method of Degen.pl
+#'
+"degenSZ"
