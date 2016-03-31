121ad4e0c9c5d143282bf14f4d733eb7019919da
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-03-30 21:11:05 -0400

    updates

--- data-raw/makeprimertables.R
+++ data-raw/makeprimertables.R
@@ -2,6 +2,6 @@
 source("degeneracy_tables.R")
 
 #save them as package data
-devtools::use_data(degenS)
-devtools::use_data(degenSZ)
-devtools::use_data(degenZ)
+devtools::use_data(degenS,  overwrite = TRUE)
+devtools::use_data(degenSZ, overwrite = TRUE)
+devtools::use_data(degenZ,  overwrite = TRUE)
