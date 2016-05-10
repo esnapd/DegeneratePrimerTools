1bf003c3c3b37b11d13fd4afcadb85ff160d0ff7
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-05-10 12:26:04 -0400

    start testing

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(DegeneratePrimerTools)
+
+test_check("DegeneratePrimerTools")
---
+++ tests/testthat/test_DegeneracyCodonTables.R
@@ -0,0 +1,23 @@
+library(DegeneratePrimerTools)
+context('Checking the Codong Tables')
+
+test_that("the 3 basic degeneracy tables work", {
+  expect_equal(makedegenerateseqs("ACT"), "ACN")
+  expect_equal(makedegenerateseqs("AGG", method="S"), "MGN")
+  expect_equal(makedegenerateseqs("AGG", method="Z"), "CGN")
+  expect_equal(makedegenerateseqs("AGG", method="SZ"), "MGN")
+
+  expect_equal(makedegenerateseqs("TCG", method="SZ"), "NNN")
+  expect_equal(makedegenerateseqs("TCR", method="SZ"), "NNN")
+  expect_equal(makedegenerateseqs("TCY", method="SZ"), "NNN")
+  expect_equal(makedegenerateseqs("TCW", method="SZ"), "NNN")
+
+})
+
+test_that("make degenerateseqs can hancdle multiple types of text", {
+  expect_equal(makedegenerateseqs("ACT"), "ACN")
+  expect_equal(makedegenerateseqs("ACTACT"), "ACNACN")
+  expect_equal(makedegenerateseqs("ACTACTT"), "ACNACNT")
+  expect_equal(makedegenerateseqs(c("ACTACTT","ACTACTT")), c("ACNACNT","ACNACNT"))
+
+})
