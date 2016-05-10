782626172b457b3ac8eed4b91179cdb756e02512
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-05-10 12:08:34 -0400

    make codon degenerate function to make sequences degenerate according to the S/Z/SZ degen codon tables.

--- R/Degen.R
+++ R/Degen.R
@@ -12,5 +12,53 @@ makedegenerate <- function(infile, method="S") {
   print(cli)
   system(cli)
 }
+#' make codons degenerate
+#'
+#' use the Degen synonymous and nonsynonymous mutations
+#'
+#' @param sequences
+#' @param method
+#' @importFrom purrr map_chr
+#' @export
+#'
+makedegenerateseqs <- function(sequences, method="S") {
+  if (!method %in% c("S","Z","SZ")) stop("Degeneracy method is only one one of 'S','Z'. or 'SZ'")
+  if (!is.character(sequences)) stop("makedegenerate requires characters as input")
 
-paste0("'","S","'")
+  data("degenS")
+  data("degenZ")
+  data("degenSZ")
+
+  if (method == "S") {
+    deglist <- degenS
+  } else if (method == "Z") {
+    deglist <- degenZ
+  } else if  (method == "SZ") {
+    deglist <- degenSZ
+  }
+
+  if (length(sequences) == 1) {
+    # handle single string
+    if (nchar(sequences) == 3) {
+      return( as.character( deglist[sequences] ))
+    } else if (nchar(sequences) < 3 ){
+      return(sequences)
+    } else if (nchar(sequences) > 3 ) {
+      #get triplets, get deenerate codons, return a single string
+      codons <- strsplit(gsub("([[:alnum:]]{3})", "\\1 ", sequences), " ")[[1]]
+      degcodons <- map_chr(codons, function(x) {makedegenerateseqs(x, method = method)})
+      return( paste(degcodons, collapse = ""))
+    }
+  } else {
+    # handle a series of strings
+    return(map_chr(sequences, function(x){makedegenerateseqs(x, method = method)}))
+  }
+}
+makedegenerateseqs("ATGACTCTGGTCG")
+makedegenerateseqs(c("ATGACTCTGGTCG","ACTGTGTGCATGCTGATC"), method="Z")
+# length("ACT")
+# is.character(c("ASC","CASC"))
+# is.character("CASC")
+# makedegenerate("ACT")
+# makedegenerate(c("ACT","TCA"))
+# paste0("'","S","'")
