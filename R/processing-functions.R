f7c37b09564fbb328c654b62c21b126fd31fa6aa
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-05-13 15:05:01 -0400

    treebuilding on deeprime and finx the find_primers

--- R/processing-functions.R
+++ R/processing-functions.R
@@ -1,23 +1,67 @@
-#' Visualize the coverage of a primer on an Interactive Pylogenetic Tree
+#' Run a Multiple Sequence Alignment
+#'
+#' @importFrom msa msa
 #' @export
-visualize_coverage <- function(matchdf, tree) {
+run_alignment <- function(dgprimer, method="ClustalW", maxiters="default", force=FALSE,...) {
+
+  if (force==FALSE  & !is.null(dgprimer@msa)) {
+    stop("Your degeprime object already has an MSA. To overwrite use force=TRUE")
+  }
+
+  seqs <- dgprimer@refseq
+  aln  <- msa::msa(seqs, method=method, maxiters=maxiters, type="dna", ...)
+
+  dgprimer@msa <- as(aln, "DNAMultipleAlignment")
+  return(dgprimer)
+}
+#' Create a Tree from An MSA
+#'
+#' @importFrom ape bionjs as.DNAbin dist.dna
+#' @export
+build_tree <- function(dgprimer, method="ClustalW", maxiters="default", force=FALSE,...) {
+
+  if (is.null(dgprimer@msa)) {
+    stop("Your degeprime object does not have a MultipleSequenceAlignment. Try using run-alignment")
+  }
+  if (force==FALSE  & !is.null(dgprimer@phy_tree)) {
+    stop("Your degeprime object already has an MSA. To overwrite use force=TRUE")
+  }
+
+  aln     <- as.DNAbin(dgprimer@msa)
+  nucdist <- dist.dna(aln)
+  tree    <- bionjs(nucdist)
+
+  dgprimer@phy_tree <- tree
+
+  return(dgprimer)
+}
+#' Visualize the coverage of a primer on an Interactive Pylogenetic Tree
+#'
+#' @importFrom purrr map_chr
+#' @export
+visualize_coverage <- function(matched,tree) {
   # note this funciton relies on the consistent ordering of nodes
   # in phylo objects where tips come first, then nodes in the edgelist
+  # https://sites.google.com/site/phylogeneticworkshop2/labs/plotting-phylogenies
+
+  #matches <- dgprimer@primerdata
+  #tree    <- dgprimer@phy_tree
+
 
   #names of primer pairs having a match then get the  tip indices
   matches  <- matchdf[!is.na(matchdf$expected),]$sequence
   matchidx <- which(tree$tip.label %in% matches)
 
   #get nodes attached to matched tips
-  connectednodes <- tree$edge[ tree$edge[,2] %in% matchidx, ]
-  connectednodes <- connectednodes[,1] #mat->vec
+  connectednodes    <- tree$edge[ tree$edge[,2] %in% matchidx, ]
+  connectednodes    <- connectednodes[,1] #mat->vec
   connectednodelabs <- connectednodes - length(tree$tip.label)
 
   print(connectednodelabs)
   tree$node.label[c(2:length(tree$node.label))] <- "No_Match"
-  tree$node.label[c(connectednodelabs)] <- "Primer_Match"
+  tree$node.label[c(connectednodelabs)]         <- "Primer_Match"
 
-  tree$tip.label <- purrr::map_chr(tree$tip.label, function(x){strsplit(x, "_")[[1]][[1]]})
+  tree$tip.label <- map_chr(tree$tip.label, function(x){strsplit(x, "_")[[1]][[1]]})
 
   phylogenetictree(tree,colordomain=c("Primer_Match", "No_Match"))
   return(tree)
@@ -47,38 +91,37 @@ updateTreePrimerMatch <- function(matchdf ,tree) {
 }
 #' Find Primers matching A Set of Reference Sequences
 #'
-#' @importFrom Biostrings matchPattern
+#' @importFrom Biostrings matchPattern reverseComplement
 #' @export
-find_primers <- function(fp, rp, refseq) {
-  if (!class(fp) == "DNAString") {
-    stop("Expecting a DNAString as the reference object")
-  }
-  if (!class(rp) == "DNAString") {
-    stop("Expecting a DNAString as the reference object")
-  }
-  if (!class(refseq) == "DNAStringSet") {
-    stop("Expecting a DNAString as the reference object")
-  }
-
+setGeneric("find_primers", function(refseq, fp, rp ) standardGeneric("find_primers"))
+#'
+setMethod("find_primers", c("DNAString", "DNAString", "DNAString"), function(refseq, fp, rp ) {
   # determine if the FP/RP match the sequences.
-  primermatches <- lapply(refseq, function(x){
-    p1    <- matchPattern(pattern=fp, subject=x, fixed=FALSE)
-    p2    <- matchPattern(pattern=rc(rp), subject=x, fixed=FALSE)
-    p1loc <- start(p1)[1]
-    p2loc <- start(p2)[1]
-    if (length(p1) == 0) warning("No Matches for the Forward Primer")
-    if (length(p2) == 0) warning("No Matches for the Reverse Primer")
-    if (length(p1) >  1) warning("Multiple matches for the forward primer, using the first.")
-    if (length(p2) >  1) warning("Multiple matches for the forward primer, using the first.")
-    return(data.frame(start=p1loc,end=p2loc))
-  })
+  p1    <- matchPattern(pattern=fp, subject=x, fixed=FALSE)
+  p2    <- matchPattern(pattern=rc(rp), subject=x, fixed=FALSE)
+  p1loc <- start(p1)[1]
+  p2loc <- start(p2)[1]
+  if (length(p1) == 0) warning("No Matches for the Forward Primer")
+  if (length(p2) == 0) warning("No Matches for the Reverse Primer")
+  if (length(p1) >  1) warning("Multiple matches for the forward primer, using the first.")
+  if (length(p2) >  1) warning("Multiple matches for the forward primer, using the first.")
 
-  # combine and calculate stats
-  df          <- Reduce(rbind, primermatches)
+  df          <- data.frame(start=p1loc,end=p2loc)
   df$expected <- df$end - df$start
   df$sequence <- names(primermatches)
   return(df)
-}
+})
+#'
+#'
+setMethod("find_primers", c("DNAStringSet", "DNAString", "DNAString"), function(refseq, fp, rp ) {
+  # determine if the FP/RP match the sequences.
+  primermatches <- lapply(refseq, function(x,fp=fp,rp=rp){
+    return(find_primers(x, fp, rp))
+  })
+  # combine and calculate stats
+  df          <- Reduce(rbind, primermatches)
+  return(df)
+})
 #' Split FNA by Tree Distance And run Degenera On Each Subset
 #'
 #' @import ape
@@ -112,7 +155,6 @@ split_fna_tree <- function(tree,fnas, splits=2, degeneracyrange=c(1,4,10,50,100,
                  oligolength = oligolength)
     primdata$subtree <- paste0("Subtree_", i)
     return(primdata)})
-
 
    return(list(fnas=subfnas, primerdata=primerdata))
 
