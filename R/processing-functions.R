432bea7487714fa498b65f309f7d7846d7b5620d
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-05-15 16:54:20 -0400

    add primer choosing function

--- R/processing-functions.R
+++ R/processing-functions.R
@@ -105,3 +105,61 @@ run_degeprime <- function(alignmentfile, oligolength, degeneracyrange=c(1,4,100,
 
   return(aggdata)
 }
+#' choose a primer from the DEGEPRIMER output
+#'
+#' obtain the peaks from a DEGEPRIME dataframe
+#'
+#' @import miniUI
+#' @import ggplot2
+#' @importFrom  shiny plotOutput runGadget dialogViewer fillRow checkboxGroupInput
+#' @export
+setGeneric("choose_primer", function(obj) standardGeneric("choose_primer"))
+#'
+#' @export
+setMethod("choose_primer", "degeprimer", function(object){choose_primer(object@primerdata)})
+#'
+#' @export
+setMethod("choose_primer", "primerdata", function(object){
+  df <- as.data.frame(object)
+
+  ui <- miniPage(
+    gadgetTitleBar("Drag to Select Primer", right = miniTitleBarButton("done", "Done", primary = TRUE)),
+
+    miniContentPanel(
+      fillRow(flex = c(1, 3),
+
+              #degeneracy values
+              checkboxGroupInput('show_degen', 'Primer degeneracy values to show:',
+                                 unique(df$degeneracy), selected = max(unique(df$degeneracy))),
+
+              # The brush="brush" argument means we can listen for
+              # brush events on the plot using input$brush.
+              plotOutput("plot", height = "100%", brush = "brush")
+      )))
+
+
+  server <- function(input, output, session) {
+    # Render the plot
+    output$plot <- renderPlot({
+      # Plot the data with x/y vars indicated by the caller.
+      ggplot(df[df$degeneracy %in% input$show_degen,] , aes(Pos, coverage, color=PrimerDeg)) + geom_point()
+    })
+
+    # Handle the Done button being pressed.
+    observeEvent(input$done, {
+      # Return the brushed points. See ?shiny::brushedPoints.
+      stopApp(brushedPoints(data, input$brush))
+    })
+    # Handle the Done button being pressed.
+    observeEvent(input$done, {
+      # Return the brushed points. See ?shiny::brushedPoints.
+      stopApp(brushedPoints(df, input$brush))
+    })
+  }
+  runGadget(ui,
+            server,
+            viewer =  dialogViewer("Pick Points",
+                                   width = 1000,
+                                   height = 700))
+
+})
--- R/visualization-functions.R
+++ R/visualization-functions.R
@@ -1,45 +1,3 @@
-#' peakfinder
-#'
-#' obtain the peaks from a DEGEPRIME dataframe
-#'
-#' @import miniUI
-#' @import ggplot2
-#' @importFrom  shiny plotOutput runGadget dialogViewer
-#' @export
-peakfinder <- function(data, xvar, yvar, width=700, height=400){
-  ui <- miniPage(
-    gadgetTitleBar("Drag to Select", right = miniTitleBarButton("done", "Done", primary = TRUE)),
-
-    miniContentPanel(
-      # The brush="brush" argument means we can listen for
-      # brush events on the plot using input$brush.
-      plotOutput("plot", height = "100%", brush = "brush")
-    ))
-
-  server <- function(input, output, session) {
-    #observeEvent(input$done, { stopApp(input$moleculedata) })
-
-    # Render the plot
-    output$plot <- renderPlot({
-      # Plot the data with x/y vars indicated by the caller.
-      ggplot(data, aes_string(xvar, yvar)) + geom_point()
-    })
-
-    # Handle the Done button being pressed.
-    observeEvent(input$done, {
-      # Return the brushed points. See ?shiny::brushedPoints.
-      stopApp(brushedPoints(data, input$brush))
-    })
-
-  }
-
-  runGadget(ui,
-            server,
-            viewer =  dialogViewer("Pick Points",
-                                   width = width,
-                                   height = height))
-
-}
 #' Plot the output from rundegeprime
 #'
 #' @import ggplot2
@@ -58,7 +16,6 @@ plot_degeprimer <- function(degedf) {
     aes(label = Pos)
   )
 }
-
 #' Plot how well a primerset covers a tree.
 #'
 #' note this function relies on the consistent ordering of nodes
