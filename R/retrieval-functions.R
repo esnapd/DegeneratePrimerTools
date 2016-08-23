1f35cc2674cb630bfbf139f08f5937d196d6c4e9
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-08-23 17:52:21 -0400

    Add retrieval functions to obtain FNA sequences for a given PFAM directory

---
+++ R/retrieval-functions.R
@@ -0,0 +1,131 @@
+#' obtain the start and stop locations for a PFAM ID
+#'
+#' Use a PFAM ID to retrieve the PFAM alignment. PFAM maintains alignments for the seed sequences as well as
+#' representative sequences (rp15, rp35, rp55, rp75), as well as sequecnes from NCBI and UNIPPROT.  The alignments are in Stockholm
+#' format and contain protein information
+#'
+#' @param pfamid. Required. A string coresponding to a PFAM accession id.
+#' @param alignmnettype. Optional. Will determine which of PFAM's prebuilt alignments to download. Can choose from "seed"
+#'             "full", "rp15", "rp35", "rp55", "rp75", "uniprot", "ncbi", "meta".
+#' @importFrom httr GET
+#' @importFrom purrr map_df
+#' @export
+#' @examples
+#' retrieve_PFAM_ids("PF16997")
+retrieve_PFAM_ids <- function(pfamid, alignmenttype = "uniprot") {
+  alignmenttypes <- c("seed", "full", "rp15", "rp35", "rp55", "rp75", "uniprot", "ncbi", "meta")
+  if (!alignmenttype %in% alignmenttypes) stop(paste("Alignment trype must bo one of", alignmenttypes))
+
+  pfamsite <- paste0("http://pfam.xfam.org/family/", pfamid ,"/alignment/", alignmenttype)
+
+  try(r <- GET(pfamsite))
+
+  if (is.null(r)) stop("There was a problem downloading the PFAM")
+
+    rows <- scan(text=content(r,"text"), what = "", sep = "\n", strip.white = TRUE, quiet = TRUE, blank.lines.skip = FALSE)
+
+  if (length(rows) < 3 || !identical(grep("^# STOCKHOLM", rows[1L]), 1L)) stop("invalid Stockholm file")
+
+  markupLines <- grep("(^\\s*|^#.*|^//\\s*)$", rows, perl=TRUE)
+  seqrows <- rows[!1:length(rows) %in% markupLines]
+  seqrows <- strsplit(seqrows, "\\s+")
+
+  df <- map_df(seqrows, function(s) {
+    id    <- strsplit(s[[1]], "/")[[1]][[1]]
+    rng   <- strsplit(s[[1]], "/")[[1]][[2]]
+    start <- strsplit(rng, "-")[[1]][[1]]
+    end   <- strsplit(rng, "-")[[1]][[2]]
+    data.frame(PFAM_ID = pfamid, Accession = id, start=start,end=end)
+  })
+
+  df$Accession <- gsub("\\..*$", "", df$Accession) #remove trailing version number
+  df
+}
+#' Get EMBL IDs from Uniprot IDs
+#'
+#' Use the REST interface at UNIPROT to get ENA mappings for a protein.
+#'
+#' @importFrom httr POST
+#' @importFrom purrr map_df
+#' @seealso \url(http://www.uniprot.org/help/programmatic_access)
+#' @export
+#' @examples
+#' retrieve_UNIPROT_to_EMBL("Q4SMD5")
+#' retrieve_UNIPROT_to_EMBL(c("Q4SMD5", "A3CRU7"))
+retrieve_UNIPROT_to_EMBL <- function(uniprotids, chunksize=200) {
+
+  baseurl <- "http://www.uniprot.org/mapping/"
+
+  if (is.character(uniprotids)) uniprotids <- c(uniprotids)
+
+  # split into groups no greater in size than the chunksize
+  groups <- split(uniprotids, ceiling(seq_along(uniprotids)/chunksize))
+
+  # batch request ID mapping using the REST API
+  df <- map_df(
+    groups,
+    function(ids) {
+      try(r <- POST(url=baseurl, query=list( from="ACC", to="EMBL", query= paste0(ids, collapse = " "), format="tab")))
+
+      if (is.null(r)) stop("There was a problem obtaining the UNIPROT->ENA Mapping")
+
+      df <- read.table(text=content(r, "text"), header = TRUE)
+      names(df) <- c("UNIPROT_ID", "EMBL_ID")
+
+      return(df)
+    }
+  )
+}
+#' Retrieve nucleotide sequences from EMBL ENA
+#'
+#' Use the REST API to retrieve sequences from EMBL
+#'
+#' @importFrom httr GET
+#' @importFrom Biostrings readDNAStringSet
+#' @importFrom Biostrings union
+#' @return a DNAStringSet
+#' @export
+#' @seealso \url("https://www.ebi.ac.uk/ena/browse/data-retrieval-rest)
+#' @examples
+#' retrieve_EMBL_sequences(c("A00145","A00146"))
+retrieve_EMBL_sequences <- function(emblids, chunksize=100) {
+
+  baseurl <- "https://www.ebi.ac.uk/ena/data/view/"
+  opts    <- "&display=fasta&download=text"
+
+  if (is.character(emblids)) emblids <- c(emblids)
+
+  if (length(emblids) == 1) {
+    r <- GET(paste0(baseurl, emblids[[1]], opts))
+    temp1 <- tempfile()
+    write(x=content(r, "text"), file=temp1)
+    dna <- readDNAStringSet(temp1)
+    return(dna)
+
+  } else {
+
+    # split into groups no greater in size than the chunksize
+    groups <- split(emblids, ceiling(seq_along(emblids)/chunksize))
+    temps <- lapply(groups, function(x){return(tempfile())})
+
+    seqs <- lapply(seq_along(groups), function(i) {
+      r <- GET(paste0(baseurl, paste0(emblids, collapse=","), opts))
+      write(x=content(r,"text"), file=temps[[i]])
+    })
+
+    dnas <- lapply(temps, readDNAStringSet)
+    return(Reduce(union, dnas))
+  }
+}
+#' A one-strop-shor for obtianing nucelotides form PFAM sequences.
+#'
+#' @export
+#' @examples
+#' retrieve_PFAM_nucleotide_sequences("PF16997")
+retrieve_PFAM_nucleotide_sequences <- function(pfamid) {
+  pfamdf <- retrieve_PFAM_ids(pfamid, alignmenttype = "uniprot")
+  uniprotids <- pfamdf$Accession
+  embldf <- retrieve_UNIPROT_to_EMBL(uniprotids)
+  seqs <- retrieve_EMBL_sequences(embldf$EMBL_ID)
+  seqs
+}
