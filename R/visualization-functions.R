045d71ed65931a2d871449c63a5167d6d642520e
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-10-10 17:09:43 -0400

    add a plot_GC funciton for visualizing GC distibution

--- R/visualization-functions.R
+++ R/visualization-functions.R
@@ -30,7 +30,7 @@ plot_coveragematrix <- function(degprim, primerpairlist=NULL, max.mismatch=1, ..
     primerpairlist <- degprim@primerpairs
   }
 
-  if (length(prmerpairlist) == 0) stop("This function requires primerpairs")
+  if (length(primerpairlist) == 0) stop("This function requires primerpairs")
   
   refseq <- degprim@refseq
 
@@ -56,4 +56,34 @@ plot_coveragematrix <- function(degprim, primerpairlist=NULL, max.mismatch=1, ..
   p  <- ggtree(degprim@phy_tree,ladderize = T)
   p  <- p + geom_tiplab(size=3, align=FALSE)
   gheatmap(p, df, ...)
+}
+#' Plot the GC Content
+#'
+#' @param degprim
+#' @importFrom ggtree gheatmap ggtree geom_tiplab
+#' @importFrom Biostrings alphabetFrequency
+#' @importFrom ggplot2 scale_fill_gradient2
+#' @export
+plot_GC <- function(degprim,  ...) {
+  if (!class(degprim) == "degeprimer") {
+    stop("The first argument must be of class 'degeprimer'")
+  }
+  
+  
+  refseq <- degprim@refseq
+  
+  alph <- Biostrings::alphabetFrequency(refseq)
+  gc   <-   mapply(FUN = function(a,c,g,t) { 
+    (g + c)/ (a + c + t + g)
+    },
+    alph[,1], alph[,2], alph[,3], alph[,4] )
+  
+  gc <- data.frame(gc)
+  row.names(gc) <- names(refseq)
+  
+  
+  # pass the created matrix to ggtree's matrix mapping function
+  p  <- ggtree(degprim@phy_tree,ladderize = T)
+  p  <- p + geom_tiplab(size=3, align=FALSE)
+  gheatmap(p, gc, ...) + scale_fill_gradient2(limits=c(0, 1), midpoint = 0.5)
 }
