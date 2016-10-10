0120586b9c9b1b78559acc1a11fa3a4b31b23e70
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-10-10 17:12:55 -0400

    ad an output function for returnign primerdata

--- R/processing-functions.R
+++ R/processing-functions.R
@@ -334,9 +334,9 @@ extract_ends <- function(dnastringset, trimf=240, trimr=175) {
 #' @importFrom purrr map_df
 #' @export
 #' 
-setGeneric("make_primer_table", function(object) standardGeneric("make_primer_table"))
+setGeneric("make_primer_table", function(object, wide) standardGeneric("make_primer_table"))
 #'
-setMethod("make_primer_table", "degeprimer", function(object) {
+setMethod("make_primer_table", "degeprimer", function(object, wide=TRUE) {
     primerpairs <- object@primerpairs
     
     if (length(primerpairs) == 0) stop("There are no primers defined. Add primers first.")
@@ -346,10 +346,26 @@ setMethod("make_primer_table", "degeprimer", function(object) {
         primername = p@name,
         forwardprimer = as.character(p@forwardprimer),
         reverseprimer = as.character(p@reverseprimer),
-        expectedlength = p@expectedsize)
+        expectedlength = p@expectedsize,
+        stringsAsFactors = FALSE)
     })
     
-    primerdata
+    if (wide) return(primerdata)
+    
+    # if tall then stack forward and reverse primers on top of one another
+    primerdataF <- primerdata
+    primerdataR <- primerdata
+    
+    primerdataF$primername <- paste0(primerdataF$primername, "_F")
+    primerdataF <- primerdataF[, c("primername", "forwardprimer")]
+    names(primerdataF) <- c("primername", "primer")
+    
+    primerdataR$primername <- paste0(primerdataR$primername, "_R")
+    primerdataR <- primerdataR[, c("primername", "reverseprimer")]
+    names(primerdataR) <- c("primername", "primer")
+    
+    return(rbind(primerdataF, primerdataR))
+
 })
 
   
