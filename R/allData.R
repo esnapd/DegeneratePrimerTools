e13015dcfdd30e16256d4412ed4959bdfbbf39a5
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-10-20 15:39:42 -0400

    add a samll degepriemr object based off of AHBA. this can be used in tests and examples

---
+++ R/allData.R
@@ -0,0 +1,21 @@
+################################################################################
+#' small Degeprime Object derived from 11 AHBA sequences
+#'
+#' 
+#' Degeprimer glues together data needed for finding and assessing degeenrate primers.
+#' This is a small degeprimer object derived from 11 AHBA sequences using the process 
+#' found in the package vignette.
+#'
+#' 
+#' @name data-ahba
+#' @aliases AHBA_degeprimer
+#' @docType data
+#' @keywords data
+#' @examples
+#' data(AHBA_degeprimer)
+#' plot_gc(AHBA_degeprime)
+#' msa <- add_primers_to_MSA(AHBA_degeprime)
+#' plot_msa(msa)
+################################################################################
+NA
+
