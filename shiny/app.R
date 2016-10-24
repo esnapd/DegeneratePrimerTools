e5919271a28a01fa413f0290655bcb0c1d607f49
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-10-24 14:18:05 -0400

    return top four conserved regions

--- shiny/app.R
+++ shiny/app.R
@@ -27,13 +27,27 @@ server <- function(input, output) {
      dp <- degeprimer(inFile()) %>%
         run_alignment() %>%
         build_tree() %>%
-        design_primers( maxdegeneracies=c(1, 10, 20), number_iterations=10)
-      
-      tabsetPanel(id = "mpanel", type = "pill"
-                  ,tabPanel("Full Alignment", renderMsaR({msaR(dp@msa)}))
-                  ,tabPanel("Region 1", renderMsaR({msaR(dp@msa)}))
-                  ,tabPanel("Region 2", renderMsaR({msaR(dp@msa)}))
-                  ,tabPanel("Region 3", renderMsaR({msaR(dp@msa)})))
+        design_primers( maxdegeneracies=c(1,10), number_iterations=12)
+     
+     #obtain the locations of peaks 
+     primerlocs <- autofind_primers(dp, keepprimers = 4)
+     
+     # Region1
+     msa1 <- add_primers_to_MSA(dp, position = primerlocs[[1]])
+     
+     msa2 <- add_primers_to_MSA(dp, position = primerlocs[[2]])
+     msa3 <- add_primers_to_MSA(dp, position = primerlocs[[3]])
+     msa4 <- add_primers_to_MSA(dp, position = primerlocs[[4]])
+     
+     # Region 4
+     tabsetPanel(
+       id = "mpanel", 
+       type = "pill",
+       tabPanel("Full Alignment", renderMsaR({msaR(dp@msa)})),
+       tabPanel("Region 1", renderMsaR({msaR(msa1)})),
+       tabPanel("Region 2", renderMsaR({msaR(msa2)})),
+       tabPanel("Region 3", renderMsaR({msaR(msa3)})),
+       tabPanel("Region 4", renderMsaR({msaR(msa4)})))
     }
   })
 }
