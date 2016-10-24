8ab3d1edb4c5396471ef9ed138efb1200450bee5
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-10-24 11:06:20 -0400

    updte shiny app to put reactive component outside of render logic. thia allows me to modify the display port to handle components

--- shiny/app.R
+++ shiny/app.R
@@ -5,22 +5,43 @@ library(msaR)
 library(dplyr)
 library(shiny)
 
+#' Server Function
+#'
 server <- function(input, output) {
-    output$msa <- renderMsaR({
-      inFile <- input$file1
-      
-      if (is.null(inFile)) return(NULL)
-      
-      dp <- degeprimer(readDNAStringSet(inFile$datapath)) %>%
+  
+  # capture the input in a reactive component
+  inFile <- reactive(
+    if (is.null(input$file1)){
+      NULL
+    }else {
+      readDNAStringSet(input$file1$datapath)
+    }
+  )
+  
+  
+  # return the display values
+  output$mainpanel <- renderUI({
+    if(is.null(inFile())) { 
+      return ()
+    } else {
+     dp <- degeprimer(inFile()) %>%
         run_alignment() %>%
         build_tree() %>%
         design_primers( maxdegeneracies=c(1, 10, 20), number_iterations=10)
       
-      
-      msaR(dp@msa) 
-    })
+      tabsetPanel(id = "mpanel", type = "pill"
+                  ,tabPanel("Full Alignment", renderMsaR({msaR(dp@msa)}))
+                  ,tabPanel("Region 1", renderMsaR({msaR(dp@msa)}))
+                  ,tabPanel("Region 2", renderMsaR({msaR(dp@msa)}))
+                  ,tabPanel("Region 3", renderMsaR({msaR(dp@msa)})))
+    }
+  })
 }
 
+#' UI Function
+#'
+#' Handles file input and proveides a generic "Main Panel" output
+#' The content of that output is controlled by the server function
 ui <- fluidPage(
     titlePanel("Uploading A Fasta File"),
     sidebarLayout(
@@ -30,10 +51,11 @@ ui <- fluidPage(
                            '.fa',
                            '.fna'))
       ),
-      mainPanel(
-        msaROutput("msa", width="100%")
-      )
+      mainPanel(uiOutput("mainpanel"))
     )
   )
 
+
+#' Call the Widget
+#'
 shinyApp(ui = ui, server = server)
