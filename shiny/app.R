0ef39c140b14b42d4b40f0d01f82a7daaa0a47c0
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-10-26 14:30:06 -0400

    add datatable and primer output tables

--- shiny/app.R
+++ shiny/app.R
@@ -1,7 +1,7 @@
 library(DegeneratePrimerTools)
 library(Biostrings)
 library(msaR)
-
+library(DT)
 library(dplyr)
 library(shiny)
 
@@ -27,7 +27,7 @@ server <- function(input, output) {
      dp <- degeprimer(inFile()) %>%
         run_alignment() %>%
         build_tree() %>%
-        design_primers( maxdegeneracies=c(1,10), number_iterations=12)
+        design_primers( maxdegeneracies=c(1,10), number_iterations=10)
      
      #obtain the locations of peaks 
      primerlocs <- autofind_primers(dp, keepprimers = 4)
@@ -35,19 +35,34 @@ server <- function(input, output) {
      # Region1
      msa1 <- add_primers_to_MSA(dp, position = primerlocs[[1]])
      
-     msa2 <- add_primers_to_MSA(dp, position = primerlocs[[2]])
-     msa3 <- add_primers_to_MSA(dp, position = primerlocs[[3]])
-     msa4 <- add_primers_to_MSA(dp, position = primerlocs[[4]])
+     msa2 <- add_primers_to_MSA(dp, position = primerlocs[[2]], strict = FALSE)
+     msa3 <- add_primers_to_MSA(dp, position = primerlocs[[3]], strict = FALSE)
+     msa4 <- add_primers_to_MSA(dp, position = primerlocs[[4]], strict = FALSE)
      
+     t1 <- data.frame(dp@primerdata) %>% filter(Pos==primerlocs[[1]])
+     t2 <- data.frame(dp@primerdata) %>% filter(Pos==primerlocs[[2]])
+     t3 <- data.frame(dp@primerdata) %>% filter(Pos==primerlocs[[3]])
+     t4 <- data.frame(dp@primerdata) %>% filter(Pos==primerlocs[[4]])
+       
+     DT::renderDataTable(t1)
      # Region 4
      tabsetPanel(
        id = "mpanel", 
        type = "pill",
-       tabPanel("Full Alignment", renderMsaR({msaR(dp@msa)})),
-       tabPanel("Region 1", renderMsaR({msaR(msa1)})),
-       tabPanel("Region 2", renderMsaR({msaR(msa2)})),
-       tabPanel("Region 3", renderMsaR({msaR(msa3)})),
-       tabPanel("Region 4", renderMsaR({msaR(msa4)})))
+       tabPanel("Full Alignment", 
+                renderMsaR({msaR(dp@msa, menu = F, alignmentHeight = 500, labelNameLength = 25)})),
+       tabPanel("Region 1",
+                renderMsaR({msaR(msa1, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                DT::renderDataTable(t1)),
+       tabPanel("Region 2",
+                renderMsaR({msaR(msa2, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                DT::renderDataTable(t2)),
+       tabPanel("Region 3",
+                renderMsaR({msaR(msa3, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                DT::renderDataTable(t3)),
+       tabPanel("Region 4",
+                renderMsaR({msaR(msa4, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                DT::renderDataTable(t4)))
     }
   })
 }
