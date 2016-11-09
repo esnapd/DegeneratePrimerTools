a1ff6752ea221fc60db7a27b557de8751214bdbc
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-11-09 14:39:18 -0500

    move all data infor to a single file

--- R/allData.R
+++ R/allData.R
@@ -19,3 +19,48 @@
 ################################################################################
 NA
 
+#' Codon List for "Z" method of Degen.pl
+#'
+#' A named list of codons corresponding to the "Z"
+#' variables of the Degen.pl algorithm.
+#'
+#'
+#' @format A named list where names are codons sequences
+#' and the values are the degenerate codon for that sequence
+#' using the "Z" method of Degen.pl
+#'
+"degenZ"
+
+#' Codon List for "S" method of Degen.pl
+#'
+#' A named list of codons corresponding to the "S"
+#' variables of the Degen.pl algorithm.
+#'
+#'
+#' @format A named list where names are codons sequences
+#' and the values are the degenerate codon for that sequence
+#' using the "S" method of Degen.pl
+#'
+"degenS"
+
+#' Codon List for "SZ" method of Degen.pl
+#'
+#' A named list of codons corresponding to the "SZ"
+#' variables of the Degen.pl algorithm.
+#'
+#'
+#' @format A named list where names are codons sequences
+#' and the values are the degenerate codon for that sequence
+#' using the "SZ" method of Degen.pl
+#'
+"degenSZ"
+
+#' p7 adaptor sequences for paired end illumina barcoding
+#'
+#' These sequences are the Illumina barcodes used for the indexing read
+#' on paired end runs. Intended for the MiSeq instrument.
+#'
+#' @format A two-column data frame containing the full p7 sequence and
+#' the barcode needed for the sample file.
+#'
+"illumina_p7"
