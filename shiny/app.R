630edddf8dc954437f06dd802ef5147808cefcbb
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-11-21 22:35:09 -0500

    update app to allow optinal degenracy values

--- shiny/app.R
+++ shiny/app.R
@@ -18,7 +18,6 @@ server <- function(input, output) {
     }
   )
   
-  
   # return the display values
   output$mainpanel <- renderUI({
     if(is.null(inFile())) { 
@@ -27,10 +26,10 @@ server <- function(input, output) {
      dp <- degeprimer(inFile()) %>%
         run_alignment() %>%
         build_tree() %>%
-        design_primers( maxdegeneracies=c(1,10), number_iterations=10)
+        design_primers(maxdegeneracies=as.numeric(input$checkGroup), number_iterations=10, ncpus = 4)
      
      #obtain the locations of peaks 
-     primerlocs <- autofind_primers(dp, keepprimers = 4)
+     primerlocs <- autofind_primers(dp, keepprimers = 5)
      
      # Region1
      msa1 <- add_primers_to_MSA(dp, position = primerlocs[[1]])
@@ -50,18 +49,18 @@ server <- function(input, output) {
        id = "mpanel", 
        type = "pill",
        tabPanel("Full Alignment", 
-                renderMsaR({msaR(dp@msa, menu = F, alignmentHeight = 500, labelNameLength = 250)})),
+                renderMsaR({msaR(dp@msa, menu = F, alignmentHeight = 300, labelNameLength = 250, seqlogo=F)})),
        tabPanel("Region 1",
-                renderMsaR({msaR(msa1, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                renderMsaR({msaR(msa1, menu = F, alignmentHeight = 300, labelNameLength = 250, seqlogo=F)}),
                 DT::renderDataTable(t1)),
        tabPanel("Region 2",
-                renderMsaR({msaR(msa2, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                renderMsaR({msaR(msa2, menu = F, alignmentHeight = 300, labelNameLength = 250, seqlogo=F)}),
                 DT::renderDataTable(t2)),
        tabPanel("Region 3",
-                renderMsaR({msaR(msa3, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                renderMsaR({msaR(msa3, menu = F, alignmentHeight = 300, labelNameLength = 250, seqlogo=F)}),
                 DT::renderDataTable(t3)),
        tabPanel("Region 4",
-                renderMsaR({msaR(msa4, menu = F, alignmentHeight = 500, labelNameLength = 250)}),
+                renderMsaR({msaR(msa4, menu = F, alignmentHeight = 300, labelNameLength = 250, seqlogo=F)}),
                 DT::renderDataTable(t4)))
     }
   })
@@ -72,13 +71,25 @@ server <- function(input, output) {
 #' Handles file input and proveides a generic "Main Panel" output
 #' The content of that output is controlled by the server function
 ui <- fluidPage(
-    titlePanel("Uploading A Fasta File"),
+    titlePanel("Upload A Fasta File to Design Degenerate Primers"),
     sidebarLayout(
       sidebarPanel(
         fileInput('file1', 'Choose a Fasta File',
                   accept=c('.fasta',
                            '.fa',
-                           '.fna'))
+                           '.fna')),
+      checkboxGroupInput(
+      "checkGroup", 
+       label = h6("Choose Degeneracy Values"), 
+       choices = list("Degeneracy 10" = 10, 
+                      "Degeneracy 50" = 50, 
+                      "Degeneracy 100" = 100,
+                      "Degeneracy 200" = 200,
+                      "Degeneracy 500" = 500,
+                      "Degeneracy 1000" = 1000,
+                      "Degeneracy 2000" = 2000),
+       selected = c(50, 100, 200, 500))
+      
       ),
       mainPanel(uiOutput("mainpanel"))
     )
