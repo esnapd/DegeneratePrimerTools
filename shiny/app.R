85a2736b1a9358f1955ffac6925b384b515be112
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-11-22 15:20:44 -0500

    Update Shiny ap with better documentation, conditional panels for file upload, and the IUPAC codes.

--- shiny/app.R
+++ shiny/app.R
@@ -4,6 +4,7 @@ library(msaR)
 library(DT)
 library(dplyr)
 library(shiny)
+library(xtable)
 
 #' Server Function
 #'
@@ -17,6 +18,14 @@ server <- function(input, output) {
       readDNAStringSet(input$file1$datapath)
     }
   )
+  
+  # this determines file upload status and is used for our UI to determine what to display
+  output$fileUploaded <- reactive({return(is.null(inFile()))})
+  outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
+  
+  output$IUPAC <- renderTable(data.frame(
+    code = names(Biostrings::IUPAC_CODE_MAP),
+    nucleotides = Biostrings::IUPAC_CODE_MAP))
   
   # return the display values
   output$mainpanel <- renderUI({
@@ -33,6 +42,7 @@ server <- function(input, output) {
      
      # Add all Peak Info to the MSA
      msa1 <- add_primers_to_MSA(dp, positions = primerlocs)
+     
      t1 <- data.frame(dp@primerdata) %>% 
        filter(Pos %in% primerlocs) %>% 
        select(Pos, PrimerSeq, PrimerDeg, degeneracy, coverage) %>%
@@ -45,11 +55,13 @@ server <- function(input, output) {
        renderMsaR({ msaR(msa1, menu = F, alignmentHeight = nrow(msa1)*rowheight, 
                         leftheader = FALSE, labelNameLength = 160, seqlogo=F)}),
        
-       DT::renderDataTable(t1, rownames = FALSE, options = list(dom = 't'))
+       DT::renderDataTable(t1, 
+                           rownames = FALSE, 
+                           options = list(
+                             dom = 't',
+                             pageLength = 5))
      )
 
-       #          renderTable(data.frame(code = names(Biostrings::IUPAC_CODE_MAP),
-       #                                 nucleotides = Biostrings::IUPAC_CODE_MAP))))
     }
   })
 }
@@ -61,25 +73,56 @@ server <- function(input, output) {
 ui <- fluidPage(
     titlePanel("Upload A Fasta File to Design Degenerate Primers"),
     sidebarLayout(
-      
       sidebarPanel(
-        fileInput('file1', 'Choose a Fasta File',
-                  accept=c('.fasta',
-                           '.fa',
-                           '.fna')),
-      checkboxGroupInput(
-      "checkGroup", 
-       label = h6("Choose Degeneracy Values"), 
-       choices = list("Degeneracy 10" = 10, 
-                      "Degeneracy 50" = 50, 
-                      "Degeneracy 100" = 100,
-                      "Degeneracy 200" = 200,
-                      "Degeneracy 500" = 500,
-                      "Degeneracy 1000" = 1000,
-                      "Degeneracy 2000" = 2000),
-       selected = c(50, 100, 200, 500))
+        
+        # Include this Conditonal Panel BEFORE File Upload
+        conditionalPanel(
+          condition="output.fileUploaded",
+          
+          p("Choose a file to upload and the degeneracy values you would like
+            to pass to DEGEPRIME. This program will desing primers against your sequences 
+            useing each of the specified values."),
+          
+          p("Higher values will generate more degenerate primers at the expense of speed."),
+          
+          checkboxGroupInput(
+            "checkGroup", 
+            label = h4("Choose Degeneracy Values"), 
+            choices = list("Degeneracy 10" = 10, 
+                           "Degeneracy 50" = 50, 
+                           "Degeneracy 100" = 100,
+                           "Degeneracy 200" = 200,
+                           "Degeneracy 500" = 500,
+                           "Degeneracy 1000" = 1000,
+                           "Degeneracy 2000" = 2000),
+            selected = c(50, 100, 200, 500)),
+          
+          hr(),
+          
+          fileInput('file1', 
+                    label=h4('Choose a Fasta File'),
+                    accept=c('.fasta', '.fa','.fna')
+          )
+        ),
       
+        conditionalPanel(
+          condition="!output.fileUploaded",
+          
+          h4("Primer Design Results"),
+          
+          p("To the right are two panels. The top displays a multiple seqeuence alignment
+            of the input sequences along with the primers designed using the degeneracy values you have specified.
+            Highlighting the schematic will navigate around the multiple sequence alignment.
+            Below is a table of primers."),
+          
+          br(),
+          
+          h4("IUPAC Degenerate Nucleotide Codes"),
+          
+          tableOutput("IUPAC")
+          )
       ),
+      
       mainPanel(uiOutput("mainpanel"))
     )
   )
