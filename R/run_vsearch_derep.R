4406bca5ba33e1365f9d58901ea860aedef697b1
Christophe Lemetre <christophelemetre@christophes-macbook-air.local.rockefeller.edu>
2016-12-02 13:16:18 -0500

    Added sampleName extraction from file name to add into log file.

--- R/run_vsearch_derep.R
+++ R/run_vsearch_derep.R
@@ -24,6 +24,7 @@ run_vsearch_derep <- function(filename, DerepFile, logfile){
   ###################################################################################################################################
   # Collect information from vsearch derep command output for number of sequences, number of unique sequences and vsearch version.
   
+  sampleName <- paste(unlist(strsplit(basename(filename), "_"))[1:2], collapse="_")
   sequ <- as.numeric(sub(".*in (.*) seqs.*", "\\1", CmdOut[grep("nt in", CmdOut)]))
   derep <- as.numeric(sub("(.*)unique sequences.*", "\\1", CmdOut[grep("unique sequences", CmdOut)]))
   version <- sub("(.*)unique sequences.*", "\\1", CmdOut[grep("vsearch", CmdOut)][1])
