92db6a42a843d20b6bc40163478a1d22d6722840
zach charlop-powers <zach.charlop.powers@gmail.com>
2016-12-22 16:21:44 -0500

    update plot degeprimer function to allow passing of degeprimer object or degepriemr dataframe

--- R/plot_degeprimer.R
+++ R/plot_degeprimer.R
@@ -1,17 +1,26 @@
-#' Plot the output from rundegeprime
+#' plot degeprimer
+#' 
+#' Plot the output from rundegeprime. THe x-axis is the location along the multiple
+#' sequence alignment and the y-axis is the coverage of the primer at that location
+#' for a given maximum degeneracy value.
 #'
+#' @param deg Required. 
+#' 
 #' @import ggplot2
 #' @importFrom zoo rollapply
 #' @importFrom ggrepel geom_text_repel
 #' @export
-plot_degeprimer <- function(degedf) {
-  cutoff <- mean(degedf$coverage) + 2*sd(degedf$coverage)
-  degedf$localmaxima <- rollapply(degedf$coverage, 9, function(x) which.max(x)==5, fill=NA)
+plot_degeprimer <- function(deg) {
+  
+  if (class(deg) ==  "degeprimer") deg <- deg@primerdata
+  
+  cutoff <- mean(deg$coverage) + 2*sd(deg$coverage)
+  deg$localmaxima <- rollapply(deg$coverage, 9, function(x) which.max(x)==5, fill=NA)
   
   #return a plot highlighting the high peaks (if there are any)
-  gg <- ggplot(degedf, aes(x=Pos,y=coverage, color=PrimerDeg)) + geom_point()
+  gg <- ggplot(deg, aes(x=Pos,y=coverage, color=PrimerDeg)) + geom_point()
   gg <- gg + facet_grid(degeneracy~.) + theme_bw()
   gg + geom_text_repel(
-    data= subset(degedf, coverage > cutoff & localmaxima == TRUE),
+    data = subset(deg, coverage > cutoff & localmaxima == TRUE),
     aes(label = Pos))
 }
