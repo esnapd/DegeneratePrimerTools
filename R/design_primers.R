1396e2604f78580abf1d5b792ab49a19756cc821
Christophe Lemetre <christophelemetre@gmail.com>
2018-06-20 16:31:47 -0400

    Correct typo

--- R/design_primers.R
+++ R/design_primers.R
@@ -1,4 +1,4 @@
-87645879#' Design Primers
+#' Design Primers
 #'
 #' A convenience function to find degenerate primers using the
 #' seqeuences of the degenerateprimers object.  Return teh resutls to
